bfe6a6fc3f95d8c875141f9b6cdf499e62199a53
lytves <lytves@gmail.com>
2017-09-17 00:26:08 +0200

    first class

--- Assets/GeekBrains/Player.cs
+++ Assets/GeekBrains/Player.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
 
 public class Player : MonoBehaviour {
+
+	public Rigidbody MyBody;
+	public float speed;
+	private Vector3 Movement;
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +17,13 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate() {
+
+		float Right = Input.GetAxisRaw("Horizontal");
+		float Forward = Input.GetAxisRaw("Vertical");
+
+		Movement.Set(Forward, 0f, Right);
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # First Person Shooter game on Unity 3D
+
+* According to the tutorial from [GeekBrains youtube chanel](https://www.youtube.com/watch?v=O01yCLKQKS8)
