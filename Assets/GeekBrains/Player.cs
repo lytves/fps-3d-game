21bfe23ee72eea0f28d2d6d3f84d2ba2376bc652
lytves <lytves@gmail.com>
2017-09-17 22:28:07 +0200

    player

--- Assets/GeekBrains/Player.cs
+++ Assets/GeekBrains/Player.cs
@@ -5,32 +5,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public Rigidbody MyBody;
+	public Rigidbody myBody;
 	public float speed;
-	private Vector3 Movement;
+	private Vector3 movement;
+	public GameObject bullet, startBullet;
 
 	// Use this for initialization
 	void Start ()
 	{
-		MyBody = GetComponent<Rigidbody>();
+		myBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		if (Input.GetMouseButtonDown(0))
+		{
+			Fire();
+		}
 	}
 
 	//method for move the player
 	void FixedUpdate()
 	{
+		float right = Input.GetAxisRaw("Horizontal");
+		float forward = Input.GetAxisRaw("Vertical");
 
-		float Right = Input.GetAxisRaw("Horizontal");
-		float Forward = Input.GetAxisRaw("Vertical");
+		movement.Set(forward, 0f, right);
 
-		Movement.Set(Forward, 0f, Right);
-
-		MyBody.AddForce(transform.forward * Forward * speed, ForceMode.Impulse);
-		MyBody.AddForce(transform.right * Right * speed, ForceMode.Impulse);
+		myBody.AddForce(transform.forward * forward * speed, ForceMode.Impulse);
+		myBody.AddForce(transform.right * right * speed, ForceMode.Impulse);
 	}
-}
+
+	//method for fire
+	void Fire()
+	{
+		//clone the object "bullet" 
+		Instantiate(bullet, startBullet.transform.position, startBullet.transform.rotation);
+	}
+} 
