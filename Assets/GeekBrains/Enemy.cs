a068376c9e770dc5aaaea4281af4aed665154b67
lytves <lytves@gmail.com>
2017-09-17 22:27:51 +0200

    enemy

---
+++ Assets/GeekBrains/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int health;
+	Rigidbody myBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void takeDamage(int Damage)
+	{
+		health--;
+		if(health <= 0)
+		{
+			myBody.constraints = RigidbodyConstraints.None;
+			GetComponent<MeshRenderer>().material.color = Color.red;
+		}
+	}
+}
