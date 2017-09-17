fc0107233d6aa65905975f954ca1ca32d5491d5f
lytves <lytves@gmail.com>
2017-09-17 13:08:05 +0200

    add bullet script

---
+++ Assets/GeekBrains/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+
+public class Bullet : MonoBehaviour {
+
+	public float force;
+	public int damage;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
